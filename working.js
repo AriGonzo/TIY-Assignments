c10919300129996c1c17d7b0d9802f81c21ffb63
Ariel Gonzalez <ArielGonzalez515@gmail.com>
2014-10-01 23:14:33 -0400

    DONE

--- working.js
+++ working.js
@@ -13,30 +13,30 @@ hundreds = ["zero", "one hundred ", "two hundred ", "three hundred ", "four hund
             "five hundred ", "six hundred ", "seven hundred ", "eight hundred ",
             "nine hundred "]
 
-thousands = ["zero", "one thousand ", "two thousand ", "three thousand ", "four thousand",
+thousands = ["zero", "one thousand ", "two thousand ", "three thousand ", "four thousand ",
              "five thousand ", "six thousand ", "seven thousand ", "eight thousand ",
-             "nine thousand"]
+             "nine thousand "]
 
   function num2letter(num) {
-    var cents = ((num - Math.floor(num)).toFixed(2) * 100)
-    num = Math.floor(num)
-    var val = num.toString();
+    var cents = ((num - Math.floor(num))* 100).toFixed(0) //calculates the change
+    num = Math.floor(num) //rounds the number down to a whole number
+    var val = num.toString(); //converts the number to an array of strings
 
     if (num < 20){
     return ones[num] + " dollars and " + cents + "/100 cents";
   }
     if (num < 100) {
-      if (num % 10 === 0) {
+      if (num % 10 === 0) { //fixes the issue of numbers like 30 != thirty zero
       return tens[val[0]] + "dollars and " + cents + "/100 cents";
     } {
       return tens[val[0]] + ones[val[1]] + " dollars and " + cents + "/100 cents";
     }
   }
   if (num < 1000) {
-    if (val[1] === '0' && val[2] === '0') {
+    if (val[1] === '0' && val[2] === '0') { //fixes the issue to get numbers like 100 to work
     return hundreds[val[0]] + "dollars and " + cents + "/100 cents";
     }
-    if (val[1] < 2) {
+    if (val[1] < 2) { //fixes issues like 'one hundred ten two' != one hundred twelve
     return hundreds[val[0]] + teens[val[2]] + " dollars and " + cents + "/100 cents";
     }
     if (num % 10 === 0) {
@@ -45,9 +45,20 @@ thousands = ["zero", "one thousand ", "two thousand ", "three thousand ", "four 
       return hundreds[val[0]] + tens[val[1]] + ones[val[2]] + " dollars and " + cents + "/100 cents";
     }
   }
+  if (num < 10000) {
+    if(val[1] === '0' && val[2] === '0' && val[3] === '0') {
+      return thousands[val[0]] + "dollars and " + cents + "/100 cents";
+    }
+  if (val[2] < 2) {
+    return thousands[val[0]] + hundreds[val[1]] + teens[val[3]] + " dollars and " + cents + "/100 cents";
+  } {
+    return thousands[val[0]] + hundreds[val[1]] + tens[val[2]] + ones[val[3]] + " dollars and " + cents + "/100 cents";
+    }
+  }
 }
-
-
+console.log(num2letter(1234.56))
+console.log(num2letter(1100))
+console.log(num2letter(1119.20))
 console.log(num2letter(12.50))
 console.log(num2letter(19))
 console.log(num2letter(0))
@@ -73,3 +84,8 @@ console.log(num2letter(200))
 console.log(num2letter(300))
 console.log(num2letter(400))
 console.log(num2letter(500))
+console.log(num2letter(1000))
+console.log(num2letter(2000))
+console.log(num2letter(3000))
+console.log(num2letter(4000))
+console.log(num2letter(5000))
