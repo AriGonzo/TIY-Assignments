8fc5481dc7a31e379df43e6e74a2f346bc131482
Ariel Gonzalez <ArielGonzalez515@gmail.com>
2014-10-06 20:07:05 -0400

    attempt at forEach loop

--- 08--Conway-LITE.js
+++ 08--Conway-LITE.js
@@ -50,14 +50,11 @@ function neighborOf(board, x, y) {
 
 function conway(cell, neighbors) {
   var livingCell = 0;
-  var neighbors = neighborOf(board, x, y);
-  for (var x = 0; x < board.length; x++ ) {
-    for (var y = 0; y < board[x].length; y++){
-      for (var i = 0; i < neighbors.length; i++){
-        if (neighbors[i] === true)
-           livingCell++;
-      }         //applying the rules by counting LivingCell Neighbors
-        if (cell === true){    //rule #1
+  neighbors.forEach(function(value, index){
+    if (value === true){
+      livingCell++;    //applying the rules by counting LivingCell Neighbors
+    }        //applying the rules by counting LivingCell Neighbors
+        if (cell === true) {    //rule #1
           if (livingCell < 2) {
             newCell = false;
         }
@@ -72,25 +69,24 @@ function conway(cell, neighbors) {
             newCell = true;
        } else {
             newCell = false;
-        }
-       }
+        };
+      };
        return newCell
-     }
-   }
+     });
 }
 
 //TICK CODE***
 
 function tick(board){
   var newBoard = [] //create an empty array for the new board
-  board.forEach(function(row, i) { //walks along the rows of board
-    row.forEach(function(cell, j) { //walks along columns of board
-      newBoard = conway(board[i][j], neighborOf(board, i, j));
-    });                     //running conway and neighborOf functions and pushing
-  });                       //them into an array.
-  // var row1 = newBoard.splice(0, 3);//splicing the array into 3 parts
-  // var row2 = newBoard.splice(0, 3);
-  // board = [row1, row2, newBoard]; //returning the full assembled board
+  for (var i = 0; i < board.length; i++) { //walks along the rows of board
+    for (var j = 0; j < board[i].length; j++) { //walks along columns of board
+      newBoard.push(conway(board[i][j], neighborOf(board, i, j)));
+    }                     //running conway and neighborOf functions and pushing
+  }                       //them into an array.
+  var row1 = newBoard.splice(0, 3);//splicing the array into 3 parts
+  var row2 = newBoard.splice(0, 3);
+  board = [row1, row2, newBoard]; //returning the full assembled board
   return board;
 }
 
@@ -159,12 +155,12 @@ var board6   = [
 
 
 //TESTS
-// tick_test(board);
-// tick_test(board2);
-// tick_test(board3);
-// tick_test(board4);
+tick_test(board);
+tick_test(board2);
+tick_test(board3);
+tick_test(board4);
 tick_test(board5);
-// tick_test(board6);
+tick_test(board6);
 // console.log(board);
 // console.log(tick(board));
 // console.log(board2);
