2acfc174f721febcefe8d4984af69e9d6354c517
Ariel Gonzalez <ArielGonzalez515@gmail.com>
2014-10-10 14:48:44 -0400

    changed String Calc to be object oriented

--- 07--Check-Writing.js
+++ 07--Check-Writing.js
@@ -47,48 +47,52 @@ thousands = ["zero", "one thousand ", "two thousand ", "three thousand ", "four 
              "five thousand ", "six thousand ", "seven thousand ", "eight thousand ",
              "nine thousand "] //this array holds all of the thousands values to be concatenated later
 
-  function num2letter(num) {
-    var cents = ((num - Math.floor(num))* 100).toFixed(0) //calculates the change
-    num = Math.floor(num) //rounds the number down to a whole number
-    var val = num.toString(); //converts the number to an array of strings
+var checkWriting ={
+    num2letter: function(num){
+        var cents = ((num - Math.floor(num))* 100).toFixed(0) //calculates the change
+        num = Math.floor(num) //rounds the number down to a whole number
+        var val = num.toString(); //converts the number to an array of strings
 
-    if (num < 20){ //values 0-20
-    return ones[num] + " dollars and " + cents + "/100 cents";
-  }
-    if (num < 100) { //values 20 - 99
-      if (num % 10 === 0) { //fixes the issue of numbers like 30 != thirty zero
-      return tens[val[0]] + "dollars and " + cents + "/100 cents";
-    } {
-      return tens[val[0]] + ones[val[1]] + " dollars and " + cents + "/100 cents";
+        if (num < 20){ //values 0-20
+        return ones[num] + " dollars and " + cents + "/100 cents";
+      }
+        if (num < 100) { //values 20 - 99
+          if (num % 10 === 0) { //fixes the issue of numbers like 30 != thirty zero
+          return tens[val[0]] + "dollars and " + cents + "/100 cents";
+        } {
+          return tens[val[0]] + ones[val[1]] + " dollars and " + cents + "/100 cents";
+        }
+      }
+      if (num < 1000) { //values 100-999
+        if (val[1] === '0' && val[2] === '0') { //fixes the issue to get numbers like 100 to work
+        return hundreds[val[0]] + "dollars and " + cents + "/100 cents";
+        }
+        if (val[1] < 2) { //fixes issues like 'one hundred ten two' != one hundred twelve
+        return hundreds[val[0]] + teens[val[2]] + " dollars and " + cents + "/100 cents";
+        }
+        if (num % 10 === 0) {
+          return hundreds[val[0]] + tens[val[1]] + "dollars and " + cents + "/100 cents";
+        } {
+          return hundreds[val[0]] + tens[val[1]] + ones[val[2]] + " dollars and " + cents + "/100 cents";
+        }
+      }
+      if (num < 10000) { //values 1000 - 9999
+        if(num % 100 === 0) {
+          return thousands[val[0]] + hundreds[val[1]] + "dollars and " + cents + "/100 cents";
+        }
+        if(val[1] === '0' && val[2] === '0' && val[3] === '0') {
+          return thousands[val[0]] + " dollars and " + cents + "/100 cents";
+        }
+      if (val[2] < 2) {
+        return thousands[val[0]] + hundreds[val[1]] + teens[val[3]] + " dollars and " + cents + "/100 cents";
+      } {
+        return thousands[val[0]] + hundreds[val[1]] + tens[val[2]] + ones[val[3]] + " dollars and " + cents + "/100 cents";
+        }
+      }
     }
   }
-  if (num < 1000) { //values 100-999
-    if (val[1] === '0' && val[2] === '0') { //fixes the issue to get numbers like 100 to work
-    return hundreds[val[0]] + "dollars and " + cents + "/100 cents";
-    }
-    if (val[1] < 2) { //fixes issues like 'one hundred ten two' != one hundred twelve
-    return hundreds[val[0]] + teens[val[2]] + " dollars and " + cents + "/100 cents";
-    }
-    if (num % 10 === 0) {
-      return hundreds[val[0]] + tens[val[1]] + "dollars and " + cents + "/100 cents";
-    } {
-      return hundreds[val[0]] + tens[val[1]] + ones[val[2]] + " dollars and " + cents + "/100 cents";
-    }
-  }
-  if (num < 10000) { //values 1000 - 9999
-    if(num % 100 === 0) {
-      return thousands[val[0]] + hundreds[val[1]] + "dollars and " + cents + "/100 cents";
-    }
-    if(val[1] === '0' && val[2] === '0' && val[3] === '0') {
-      return thousands[val[0]] + " dollars and " + cents + "/100 cents";
-    }
-  if (val[2] < 2) {
-    return thousands[val[0]] + hundreds[val[1]] + teens[val[3]] + " dollars and " + cents + "/100 cents";
-  } {
-    return thousands[val[0]] + hundreds[val[1]] + tens[val[2]] + ones[val[3]] + " dollars and " + cents + "/100 cents";
-    }
-  }
-}
+
+console.log(checkWriting.num2letter(423))
 
 
 //
@@ -119,76 +123,76 @@ thousands = ["zero", "one thousand ", "two thousand ", "three thousand ", "four 
 //   [130, "one hundred thirty dollars and 0/100 cents"],
 //   [321.92, "three hundred twenty one dollars and 92/100 cents"]
 // ]
-var assert = require('assert');
-
-suite(num2letter(0), function(){
-  var pre = "should return check amount when given";
-  test('should return check amount when given "0"', function(){
-    assert.equal("zero dollars and 0/100 cents", num2letter(0))
-  });
-  test(pre + " 1234.56", function(){
-    assert.equal("one thousand two hundred thirty four dollars and 56/100 cents", num2letter(1234.56))
-  });
-  test(pre +  " 1100", function(){
-    assert.equal("one thousand one hundred dollars and 0/100 cents", num2letter(1100))
-  });
-  test(pre +  " 1119.20", function(){
-    assert.equal("one thousand one hundred nineteen dollars and 20/100 cents", num2letter(1119.20))
-  });
-  test(pre + " 321.92", function(){
-    assert.equal("three hundred twenty one dollars and 92/100 cents", num2letter(321.92))
-  });
-  test(pre + " 30.12", function(){
-    assert.equal("thirty dollars and 12/100 cents", num2letter(30.12))
-  });
-  test(pre + " 112", function(){
-    assert.equal("one hundred twelve dollars and 0/100 cents", num2letter(112))
-  });
-  test(pre + "99", function(){
-    assert.equal("ninety nine dollars and 0/100 cents", num2letter(99))
-  });
-})
-
-// testCases.forEach(function(testCase) {
-//   var actual = num2letter(testCase[0])
-//     expected = testCase[1]
-//     console.log("Number: " + testCase[0].toFixed(2) + " was converted to '" + testCase[1] + "'!")
+// var assert = require('assert');
 //
-//   assert.strictEqual(actual, expected);
-// });
+// suite(num2letter(0), function(){
+//   var pre = "should return check amount when given";
+//   test('should return check amount when given "0"', function(){
+//     assert.equal("zero dollars and 0/100 cents", num2letter(0))
+//   });
+//   test(pre + " 1234.56", function(){
+//     assert.equal("one thousand two hundred thirty four dollars and 56/100 cents", num2letter(1234.56))
+//   });
+//   test(pre +  " 1100", function(){
+//     assert.equal("one thousand one hundred dollars and 0/100 cents", num2letter(1100))
+//   });
+//   test(pre +  " 1119.20", function(){
+//     assert.equal("one thousand one hundred nineteen dollars and 20/100 cents", num2letter(1119.20))
+//   });
+//   test(pre + " 321.92", function(){
+//     assert.equal("three hundred twenty one dollars and 92/100 cents", num2letter(321.92))
+//   });
+//   test(pre + " 30.12", function(){
+//     assert.equal("thirty dollars and 12/100 cents", num2letter(30.12))
+//   });
+//   test(pre + " 112", function(){
+//     assert.equal("one hundred twelve dollars and 0/100 cents", num2letter(112))
+//   });
+//   test(pre + "99", function(){
+//     assert.equal("ninety nine dollars and 0/100 cents", num2letter(99))
+//   });
+// })
 //
-// test(true, true, 'true is true!');
-// test(num2letter(19), "nineteen dollars and 0/100 cents", '19 >>> nineteen')
-//
-//   console.log(num2letter(1234.56))
-//   console.log(num2letter(1100))
-// console.log(num2letter(1119.20))
-// console.log(num2letter(12.50))
-// console.log(num2letter(19))
-// console.log(num2letter(0))
-// console.log(num2letter(17))
-// console.log(num2letter(13))
-// console.log(num2letter(11))
-// console.log(num2letter(7))
-// console.log(num2letter(65))
-// console.log(num2letter(99))
-// console.log(num2letter(41))
-// console.log(num2letter(30.12))
-// console.log(num2letter(112))
-// console.log(num2letter(115))
-// console.log(num2letter(119))
-// console.log(num2letter(130))
-// console.log(num2letter(340))
-// console.log(num2letter(729))
-// console.log(num2letter(283))
-// console.log(num2letter(492))
-// console.log(num2letter(321.92))
-// console.log(num2letter(100))
-// console.log(num2letter(200))
-// console.log(num2letter(300))
-// console.log(num2letter(400))
-// console.log(num2letter(500))
-// console.log(num2letter(1000))
+// // testCases.forEach(function(testCase) {
+// //   var actual = num2letter(testCase[0])
+// //     expected = testCase[1]
+// //     console.log("Number: " + testCase[0].toFixed(2) + " was converted to '" + testCase[1] + "'!")
+// //
+// //   assert.strictEqual(actual, expected);
+// // });
+// //
+// // test(true, true, 'true is true!');
+// // test(num2letter(19), "nineteen dollars and 0/100 cents", '19 >>> nineteen')
+// //
+// //   console.log(num2letter(1234.56))
+// //   console.log(num2letter(1100))
+// // console.log(num2letter(1119.20))
+// // console.log(num2letter(12.50))
+// // console.log(num2letter(19))
+// // console.log(num2letter(0))
+// // console.log(num2letter(17))
+// // console.log(num2letter(13))
+// // console.log(num2letter(11))
+// // console.log(num2letter(7))
+// // console.log(num2letter(65))
+// // console.log(num2letter(99))
+// // console.log(num2letter(41))
+// // console.log(num2letter(30.12))
+// // console.log(num2letter(112))
+// // console.log(num2letter(115))
+// // console.log(num2letter(119))
+// // console.log(num2letter(130))
+// // console.log(num2letter(340))
+// // console.log(num2letter(729))
+// // console.log(num2letter(283))
+// // console.log(num2letter(492))
+// // console.log(num2letter(321.92))
+// // console.log(num2letter(100))
+// // console.log(num2letter(200))
+// // console.log(num2letter(300))
+// // console.log(num2letter(400))
+// // console.log(num2letter(500))
+// // console.log(num2letter(1000))
 // console.log(num2letter(2000))
 // console.log(num2letter(3000))
 // console.log(num2letter(4000))
--- dojo--2014-09-30.js
+++ dojo--2014-09-30.js
@@ -14,142 +14,64 @@
  * commit message.
  */
 
-function wordsToNum(word) {
-  if (word === "zero"){
-    word = 0;
-  }
-  if (word === "one"){
-    word = 1;
-  }
-  if (word === "two"){
-    word = 2;
-  }
-  if (word === "three"){
-    word = 3;
-  }
-  if (word === "four"){
-    word = 4;
-  }
-  if (word === "five"){
-    word = 5;
-  }
-  if (word === "six"){
-    word = 6;
-  }
-  if (word === "seven"){
-    word = 7;
-  }
-  if (word === "eight"){
-    word = 8;
-  }
-  if (word === "nine"){
-    word = 9;
-  }
-  if (word === "ten"){
-    word = 10;
-  }
-    return word
+var num = {
+    "zero" :  0,
+    "one" :   1,
+    "two":    2,
+    "three" : 3,
+    "four" :  4,
+    "five" :  5,
+    "six" :   6,
+    "seven" : 7,
+    "eight" : 8,
+    "nine" :  9,
+    "ten" :   10
 }
 
-// function test_wordsToNum(a, b){
-//   console.log('it should convert "' + a + '" to ' + b + ': ',
-//     wordsToNum(a) === b);
-// }
-
-function add(a, b){
-  return wordsToNum(a) + wordsToNum(b);
+var stringCalc = {
+  wordsToNum : function(wordA, wordB){
+    return num[wordA] + num[wordB];
+  }
 }
 
-// function test_add(a, b, c){
-//   console.log('It should add "' + a + '" and "' + b + '": ',
-//     add(a, b) === c);
-//   }
 
 var assert = require('chai').assert;
 
-
-suite(wordsToNum(), function() {
-  test('should return "0" when given "zero"', function(){
-    assert.equal(0, wordsToNum("zero"));
+suite("wordsToNum(), will convert two written numbers to numerals and return the sum", function() {
+  test('should return "0" when given "zero" and "zero"', function(){
+    assert.equal(0, stringCalc.wordsToNum("zero", "zero"));
   })
-  test('should return "1" when given "one"', function(){
-    assert.equal(1, wordsToNum("one"));
+  test('should return a number when given a word', function(){
+    assert.typeOf(stringCalc.wordsToNum("zero", "zero"), 'number' );
   })
-  test('should return "2" when given "two"', function(){
-    assert.equal(2, wordsToNum("two"));
+  test('should return "3" when given "one" and "two"', function(){
+    assert.equal(3, stringCalc.wordsToNum("one", "two"));
   })
-  test('should return "3" when given "three"', function(){
-    assert.equal(3, wordsToNum("three"));
+  test('should return "2" when given "one" and "one"', function(){
+    assert.equal(2, stringCalc.wordsToNum("one", "one"));
   })
-  test('should return "4" when given "four"', function(){
-    assert.equal(4, wordsToNum("four"));
+  test('should return "5" when given "three" and "two"', function(){
+    assert.equal(5, stringCalc.wordsToNum("three", "two"));
   })
-  test('should return "5" when given "five"', function(){
-    assert.equal(5, wordsToNum("five"));
+  test('should return "10" when given "four" and "six"', function(){
+    assert.equal(10, stringCalc.wordsToNum("four", "six"));
   })
-  test('should return "6" when given "six"', function(){
-    assert.equal(6, wordsToNum("six"));
+  test('should return "15" when given "five" and "ten"', function(){
+    assert.equal(15, stringCalc.wordsToNum("five", "ten"));
   })
-  test('should return "7" when given "seven"', function(){
-    assert.equal(7, wordsToNum("seven"));
+  test('should return "6" when given "six" and "zero"', function(){
+    assert.equal(6, stringCalc.wordsToNum("six", "zero"));
   })
-  test('should return "8" when given "eight"', function(){
-    assert.equal(8, wordsToNum("eight"));
+  test('should return "11" when given "seven" and "four"', function(){
+    assert.equal(11, stringCalc.wordsToNum("seven", "four"));
   })
-  test('should return "9" when given "nine"', function(){
-    assert.equal(9, wordsToNum("nine"));
+  test('should return "9" when given "eight" and "one"', function(){
+    assert.equal(9, stringCalc.wordsToNum("eight", "one"));
   })
-  test('should return "10" when given "ten"', function(){
-    assert.equal(10, wordsToNum("ten"));
+  test('should return "9" when given "three" and "six"', function(){
+    assert.equal(9, stringCalc.wordsToNum("three", "six"));
   })
-})
-
-suite(add(), function() {
-  test('should return 0 as sum of "0" and "0"', function(){
-    assert.equal(0, add(0, 0));
-  });
-  test('should return 1 as sum of "0" and "1"', function(){
-    assert.equal(1, add(0, 1));
-  })
-  test('should return 6 as sum of "4" and "2"', function(){
-    assert.equal(6, add(4, 2));
-  })
-  test('should return 10 as sum of "5" and "5"', function(){
-    assert.equal(10, add(5, 5));
-  })
-  test('should return 20 as sum of "10" and "10"', function(){
-    assert.equal(20, add(10, 10));
-  })
-  test('should return 14 as sum of "9" and "5"', function(){
-    assert.equal(14, add(9, 5));
-  })
-  test('should return 18 as sum of "10" and "8"', function(){
-    assert.equal(18, add(10, 8));
-  })
-  test('should return 2 as sum of "1" and "1"', function(){
-    assert.equal(2, add(1, 1));
-  })
-  test('should return 9 as sume of "7" and "2"', function(){
-    assert.equal(9, add(7, 2));
+  test('should return "10" when given "ten" and "zero"', function(){
+    assert.equal(10, stringCalc.wordsToNum("ten", "zero"));
   })
 })
-//
-// console.log(add("three", "eight"))
-//
-// test_add("zero", "five", 5)
-// test_add("seven", "three", 10)
-// test_add("nine", "five", 14)
-// test_add("six", "eight", 14)
-// test_add("one", "one", 2)
-// test_add("seven", "two", 9)
-// test_add("five", "eight", 13)
-// test_add("three", "four", 7)
-//
-// console.log(add("one", "one") === 2)
-// console.log(add("one", "two") === 3)
-// console.log(add("one", "three") === 4)
-// console.log(add("one", "four") === 5)
-// console.log(add("one", "five") === 6)
-// console.log(add("one", "six") === 7)
-// console.log(add("one", "seven") === 8)
-// console.log(add("eight", "three") === 11)
