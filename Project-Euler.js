8c5e014c255aff8b996fee8f9ef673487f4bd1e0
Ariel Gonzalez <ArielGonzalez515@gmail.com>
2014-10-12 16:27:42 -0400

    solve problem 4 project Euler @kimkvn @arigonzo

--- Project-Euler.js
+++ Project-Euler.js
@@ -1,4 +1,4 @@
-/* === PRODUCTION CODE === */
+// /* === PRODUCTION CODE === */
 function solution(){
     // change input to suit the solution, e.g. `limit`
     return {
@@ -11,6 +11,25 @@ function solution(){
         sum: function(list){ /* . . . */ }
     }
 }
+//
+// /* === TEST CODE === */
+// // You should probably write some...
 
-/* === TEST CODE === */
-// You should probably write some...
+function solution(){
+  return{
+    mult: function() {
+      var results = [];
+      for(var x = 999; x > 100; x--) {
+        for(var y = 999; y > 100; y--) {
+          var prod = x * y;
+          if(prod.toString() == prod.toString().split("").reverse().join("")){
+            results.push(x * y);
+          }
+        }
+      }
+      return Math.max.apply(Math, results)
+    }
+  }
+}
+
+console.log(solution().mult());
