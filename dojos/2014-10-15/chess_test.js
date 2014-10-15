26c205252fbd78e6aa7d5de543000d9241cc894f
Ariel Gonzalez <ArielGonzalez515@gmail.com>
2014-10-15 17:40:56 -0400

    created chess game files and refactor 2D-Array

---
+++ dojos/2014-10-15/chess_test.js
@@ -0,0 +1,12 @@
+var Chess = require('./chess.js');
+var PosXY = require('./chess.js');
+var Piece = require('./chess.js');
+
+var assert = require('chai').assert;
+
+it('should play the game', function(){
+  var chess = new Chess;
+
+    assert.isTrue(Chess);
+
+})
