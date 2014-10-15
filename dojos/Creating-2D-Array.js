347d97d588b3276aa3b3c84d4d16b4a0262526ea
Ariel Gonzalez <ArielGonzalez515@gmail.com>
2014-10-15 19:41:50 -0400

    refactored chessboard

--- dojos/Creating-2D-Array.js
+++ dojos/Creating-2D-Array.js
@@ -38,26 +38,34 @@ var moves = {
   step4: function(){
     board[2][4] = board[1][4];
     board[1][4] = ' ';
+  },
+  //White Knight Pawn moves up 1 spot
+  step5: function(){
+    board[5][6] = board[6][6];
+    board[6][6] = ' ';
+  },
+  //Black Bishop moves diagonal down and left 1 spot
+  step6: function(){
+    board[1][4] = board[0][5];
+    board[0][5] = ' '
+  },
+  //White Bishop's Pawn moves up 2 spots
+  step7: function(){
+    board[4][2] = board[6][2];
+    board[6][2] = ' ';
+  },
+
+  //Black Knights moves over 1 spot and down 2
+  step8: function(){
+    board[2][5] = board[0][6];
+    board[0][6] = ' ';
+  },
+
+  //White Bishop moves diagonal up and right 1 spot
+  step9: function(){
+    board[6][6] = board[7][5];
+    board[7][5] = ' ';
   }
-  // //White Knight Pawn moves up 1 spot
-  // step9: board[5][6] = board[6][6],
-  // step10: board[6][6] = ' ',
-  //
-  // //Black Bishop moves diagonal down and left 1 spot
-  // step11: board[1][4] = board[0][5],
-  // step12: board[0][5] = ' ',
-  //
-  // //White Bishop's Pawn moves up 2 spots
-  // step13: board[4][2] = board[6][2],
-  // step14: board[6][2] = ' ',
-  //
-  // //Black Knights moves over 1 spot and down 2
-  // step15: board[2][5] = board[0][6],
-  // step16: board[0][6] = ' ',
-  //
-  // //White Bishop moves diagonal up and right 1 spot
-  // step17: board[6][6] = board[7][5],
-  // step18: board[7][5] = ' '
 }
 console.log(board.join('\n'));
 
@@ -69,7 +77,6 @@ var expect = require('chai').expect
 describe('Testing the board state', function(){
   it('After Move 1, the board should look like:', function(){
     moves.step1();
-
     assert.deepEqual(board,
       [
       ['R','N','B','Q','K','B','N','R'],
@@ -80,7 +87,7 @@ describe('Testing the board state', function(){
       [' ',' ',' ',' ',' ',' ',' ',' '],
       ['p','p','p',' ','p','p','p','p'],
       ['r','n','b','q','k','b','n','r'] ]
-    )
+    );
   });
   it('after Move 2, the board should look like:', function(){
     moves.step2();
@@ -95,7 +102,7 @@ describe('Testing the board state', function(){
       [' ',' ',' ',' ',' ',' ',' ',' '],
       ['p','p','p',' ','p','p','p','p'],
       ['r','n','b','q','k','b','n','r'] ]
-    )
+    );
   });
   it('after Move 3, the board should look like:', function(){
     moves.step3();
@@ -110,9 +117,11 @@ describe('Testing the board state', function(){
       [' ',' ',' ',' ',' ','n',' ',' '],
       ['p','p','p',' ','p','p','p','p'],
       ['r','n','b','q','k','b',' ','r'] ]
-    )
-  })
+    );
+  });
   it('after Move 4, the board should look like:', function(){
+    moves.step4()
+
     assert.deepEqual(board,
       [
       ['R','N','B','Q','K','B','N','R'],
@@ -123,27 +132,81 @@ describe('Testing the board state', function(){
       [' ',' ',' ',' ',' ','n',' ',' '],
       ['p','p','p',' ','p','p','p','p'],
       ['r','n','b','q','k','b',' ','r'] ]
-    )
-  })
-  // it('should be a pawn at board[2][4]', function(){
-  //   assert.equal(moves.step7, board[2][4]);
-  // })
-  // it('should be a pawn at board[5][6]', function(){
-  //   assert.equal(moves.step9, board[5][6]);
-  // })
-  // it('should be a bishop at board[1][4]', function(){
-  //   assert.equal(moves.step11, board[1][4]);
-  // })
-  // it('after step 12: board[0][5] to be blank', function(){
-  //   expect(board[0][5]).to.equal(' ');
-  // })
-  // it('should be a pawn at board[4][2]', function(){
-  //   assert.equal(moves.step13, board[4][2]);
-  // })
-  // it('should be a knight at board[2][5]', function(){
-  //   assert.equal(moves.step15, board[2][5]);
-  // })
-  // it('should be a bishop at board[6][6]', function(){
-  //   assert.equal(moves.step17, board[6][6]);
-  // })
+    );
+  });
+  it('after Move 5, the board should look like:', function(){
+    moves.step5();
+
+    assert.deepEqual(board,
+      [
+      ['R','N','B','Q','K','B','N','R'],
+      ['P','P','P',' ',' ','P','P','P'],
+      [' ',' ',' ',' ','P',' ',' ',' '],
+      [' ',' ',' ','P',' ',' ',' ',' '],
+      [' ',' ',' ','p',' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ','n','p',' '],
+      ['p','p','p',' ','p','p',' ','p'],
+      ['r','n','b','q','k','b',' ','r'] ]
+    );
+  });
+  it('after Move 6, the board should look like:', function(){
+    moves.step6();
+
+    assert.deepEqual(board,
+      [
+      ['R','N','B','Q','K',' ','N','R'],
+      ['P','P','P',' ','B','P','P','P'],
+      [' ',' ',' ',' ','P',' ',' ',' '],
+      [' ',' ',' ','P',' ',' ',' ',' '],
+      [' ',' ',' ','p',' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ','n','p',' '],
+      ['p','p','p',' ','p','p',' ','p'],
+      ['r','n','b','q','k','b',' ','r'] ]
+    );
+  });
+  it('after Move 7, the board should look like:', function(){
+    moves.step7();
+
+    assert.deepEqual(board,
+      [
+      ['R','N','B','Q','K',' ','N','R'],
+      ['P','P','P',' ','B','P','P','P'],
+      [' ',' ',' ',' ','P',' ',' ',' '],
+      [' ',' ',' ','P',' ',' ',' ',' '],
+      [' ',' ','p','p',' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ','n','p',' '],
+      ['p','p',' ',' ','p','p',' ','p'],
+      ['r','n','b','q','k','b',' ','r'] ]
+    );
+  });
+  it('after Move 8, the board should look like:', function(){
+    moves.step8();
+
+    assert.deepEqual(board,
+      [
+      ['R','N','B','Q','K',' ',' ','R'],
+      ['P','P','P',' ','B','P','P','P'],
+      [' ',' ',' ',' ','P','N',' ',' '],
+      [' ',' ',' ','P',' ',' ',' ',' '],
+      [' ',' ','p','p',' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ','n','p',' '],
+      ['p','p',' ',' ','p','p',' ','p'],
+      ['r','n','b','q','k','b',' ','r'] ]
+    );
+  });
+  it('after Move 9, the board should look like:', function(){
+    moves.step9();
+
+    assert.deepEqual(board,
+      [
+      ['R','N','B','Q','K',' ',' ','R'],
+      ['P','P','P',' ','B','P','P','P'],
+      [' ',' ',' ',' ','P','N',' ',' '],
+      [' ',' ',' ','P',' ',' ',' ',' '],
+      [' ',' ','p','p',' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ','n','p',' '],
+      ['p','p',' ',' ','p','p','b','p'],
+      ['r','n','b','q','k',' ',' ','r'] ]
+    );
+  });
 })
