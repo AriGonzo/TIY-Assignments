f0363ec7f0a04913a4cb75834745e7f0ff5a1c7e
Ariel Gonzalez <ArielGonzalez515@gmail.com>
2014-11-22 20:21:01 -0500

    working with firebase is awesome

--- YeomanProject/src/app/main/main.controller.js
+++ YeomanProject/src/app/main/main.controller.js
@@ -1,20 +1,23 @@
 'use strict';
 
-angular.module('yeomanProject')
-  .controller('MainCtrl', function ($http, $interpolate) {
-    var apiUrl = 'https://api.github.com/repos/TheIronYard--Orlando/FEE--2014--FALL/issues/417/comments'
-    var apiKeyTpl = $interpolate('?access_token={{key}}');
+angular.module('yeomanProject', ["firebase"])
+  .controller('MainCtrl', function ($firebase, $firebaseAuth) {
+    var ref = new Firebase('https://github-comments.firebaseio.com/');
+    var sync = $firebase(ref);
+    var auth = $firebaseAuth(ref);
+    var syncObject = sync.$asObject();
+
+    syncObject.$bindTo(this, "data");
+    this.messages = sync.$asArray();
+
     var self = this;
 
-    // this.apiKey = $cookies.apiKey;
-
-    this.comments = [];
-
-    $http.get(apiUrl)
-      .success(function(data){
-        self.comments = data;
-      });
-
+    auth.$authWithOAuthPopup("facebook").then(function(authData) {
+      console.log("Logged in as:", authData.uid);
+    }).catch(function(error) {
+      console.error("Authentication failed: ", error);
+    });
+    
     this.date = new Date();
 
     // this.editComment = function(event){
@@ -35,83 +38,9 @@ angular.module('yeomanProject')
       }
     }
 
-    this.addComment = function(text, apiKey){
-      var url = apiUrl + apiKeyTpl({key: apiKey})
-
-      // $cookies.apiKey = apiKey
-
-      $http.post(url, {body: text})
-        .success(function(data){
-          self.comments.push(data);
-        })
+    this.addMessage = function(newMessageText){
     }
 
-    this.awesomeThings = [
-      {
-        'key': 'angular',
-        'title': 'AngularJS',
-        'url': 'https://angularjs.org/',
-        'description': 'HTML enhanced for web apps!',
-        'logo': 'angular.png'
-      },
-      {
-        'key': 'browsersync',
-        'title': 'BrowserSync',
-        'url': 'http://browsersync.io/',
-        'description': 'Time-saving synchronised browser testing.',
-        'logo': 'browsersync.png'
-      },
-      {
-        'key': 'gulp',
-        'title': 'GulpJS',
-        'url': 'http://gulpjs.com/',
-        'description': 'The streaming build system.',
-        'logo': 'gulp.png'
-      },
-      {
-        'key': 'jasmine',
-        'title': 'Jasmine',
-        'url': 'http://jasmine.github.io/',
-        'description': 'Behavior-Driven JavaScript.',
-        'logo': 'jasmine.png'
-      },
-      {
-        'key': 'karma',
-        'title': 'Karma',
-        'url': 'http://karma-runner.github.io/',
-        'description': 'Spectacular Test Runner for JavaScript.',
-        'logo': 'karma.png'
-      },
-      {
-        'key': 'protractor',
-        'title': 'Protractor',
-        'url': 'https://github.com/angular/protractor',
-        'description': 'End to end test framework for AngularJS applications built on top of WebDriverJS.',
-        'logo': 'protractor.png'
-      },
-      {
-        'key': 'jquery',
-        'title': 'jQuery',
-        'url': 'http://jquery.com/',
-        'description': 'jQuery is a fast, small, and feature-rich JavaScript library.',
-        'logo': 'jquery.jpg'
-      },
-      {
-        'key': 'bootstrap',
-        'title': 'Bootstrap',
-        'url': 'http://getbootstrap.com/',
-        'description': 'Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.',
-        'logo': 'bootstrap.png'
-      },
-      {
-        'key': 'node-sass',
-        'title': 'Sass (Node)',
-        'url': 'https://github.com/sass/node-sass',
-        'description': 'Node.js binding to libsass, the C version of the popular stylesheet preprocessor, Sass.',
-        'logo': 'node-sass.png'
-      }
-    ];
-    angular.forEach(this.awesomeThings, function(awesomeThing) {
-      awesomeThing.rank = Math.random();
-    });
+
+
   });
