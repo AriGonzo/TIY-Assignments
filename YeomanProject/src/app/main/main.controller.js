e1a02b4dff4a3c84487ab4b8a642f79e1a847ab6
Ariel Gonzalez <ArielGonzalez515@gmail.com>
2014-11-23 00:09:30 -0500

    working comment and authentication

--- YeomanProject/src/app/main/main.controller.js
+++ YeomanProject/src/app/main/main.controller.js
@@ -1,19 +1,27 @@
 'use strict';
 
 angular.module('yeomanProject', ["firebase"])
-  .controller('MainCtrl', function ($firebase, $firebaseAuth) {
+  .controller('MainCtrl', function ($firebase, $firebaseAuth, $scope) {
     var ref = new Firebase('https://github-comments.firebaseio.com/');
     var sync = $firebase(ref);
     var auth = $firebaseAuth(ref);
     var syncObject = sync.$asObject();
-
-    syncObject.$bindTo(this, "data");
-    this.messages = sync.$asArray();
-
     var self = this;
 
+    syncObject.$bindTo($scope, "data");
+    $scope.messages = sync.$asArray();
+
+    var obj = $firebase(ref).$asObject();
+
+    // obj.$bindTo($scope, "messages").then(function() {
+    //   $scope.messages.name = self.displayName;
+    //   ref.$set({name: self.displayName});
+    // });
+
     auth.$authWithOAuthPopup("facebook").then(function(authData) {
-      console.log("Logged in as:", authData.uid);
+      console.log("Logged in as:", authData.facebook.displayName);
+      self.displayName = authData.facebook.displayName;
+      self.avatar = authData.facebook.cachedUserProfile.picture.data.url;
     }).catch(function(error) {
       console.error("Authentication failed: ", error);
     });
@@ -29,17 +37,24 @@ angular.module('yeomanProject', ["firebase"])
 
     this.commentLimit = 140;
 
-    this.timeElapsed = function(timeNow, timeStamp){
-      timeNow = Date.parse(this.date);
-      timeStamp = Date.parse(this.comments.created_at);
+    // this.timeElapsed = function(timeNow, timeStamp){
+    //   timeNow = Date.parse(this.date);
+    //   timeStamp = Date.parse(this.comments.created_at);
       
-      if(timeNow < timeStamp){
-        return true;
-      }
-    }
+    //   if(timeNow < timeStamp){
+    //     return true;
+    //   }
+    // }
 
-    this.addMessage = function(newMessageText){
-    }
+    $scope.addMessage = function(messageText, messageAuthor){
+        $scope.messages.$add({
+          'text': messageText, 
+          'author': messageAuthor,
+          'avatar': self.avatar,
+        });
+        $scope.messageText = 'BOOYA';
+        console.log($scope.messages);
+    };
 
 
 
